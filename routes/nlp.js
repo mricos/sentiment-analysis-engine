f1a69aa79e4d93e65dc6a17a39ea1e43c4e6aa4f
zoverlvx <zoverhulser@gmail.com>
2020-04-06 21:48:11 -0400

    Begins to distinguish between 200 and 400 responses

--- routes/nlp.js
+++ routes/nlp.js
@@ -11,30 +11,62 @@ const spellCorrector = new SpellCorrector();
 spellCorrector.loadDictionary();
 
 router.post("/s-analyzer", function (req, res, next) {
-	const {review} = req.body;
-	const lexedReview = aposToLexForm(review);
-	const casedReview = lexedReview.toLowerCase();
-	const alphaOnlyReview = casedReview
-		.replace(/[^a-zA-Z\s]+/g, "");
-	const {WordTokenizer} = natural;
-	const tokenizer = new WordTokenizer();
-	const tokenizedReview = tokenizer.tokenize(alphaOnlyReview);
+    const text = typeof(req.body.text) 
+        === "string" && req.body.text.length > 0
+        ? req.body.text
+        : false;
+    const key = typeof(req.body.key)
+        === "string"
+        ? req.body.key
+        : false;
+    const language = typeof(req.body.language)
+        === "string"
+        ? req.body.language
+        : false;
+	
+    if (
+        text
+        && key
+        && language
+    ) {
+        const lexedReview = aposToLexForm(text);
+	    const casedReview = lexedReview.toLowerCase();
+	    const alphaOnlyReview = casedReview
+		    .replace(/[^a-zA-Z\s]+/g, "");
+	    const {WordTokenizer} = natural;
+	    const tokenizer = new WordTokenizer();
+	    const tokenizedReview = tokenizer
+            .tokenize(alphaOnlyReview);
 
-	tokenizedReview.forEach(function(word, index) {
-		tokenizedReview[index] = spellCorrector.correct(word);
-	});
+	    tokenizedReview.forEach(function(word, index) {
+		    tokenizedReview[index] = spellCorrector.correct(word);
+	    });
 
-	const filteredReview = SW.removeStopwords(tokenizedReview);
+	    const filteredReview = SW.removeStopwords(
+            tokenizedReview
+        );
 
-	const {SentimentAnalyzer, PorterStemmer} = natural;
-	const analyzer = new SentimentAnalyzer(
-		"English", 
-		PorterStemmer, 
-		"afinn"
-	);
-	const analysis = analyzer.getSentiment(filteredReview);
+	    const {SentimentAnalyzer, PorterStemmer} = natural;
+	    const analyzer = new SentimentAnalyzer(
+		    "English", 
+		    PorterStemmer, 
+		    "afinn"
+	    );
+	    const sentiment = analyzer.getSentiment(filteredReview);
 
-	res.status(200).json({analysis});
+	    res.status(200).json({sentiment});
+    }
+    
+    // handles if no text or incorrect type
+    if (
+        !text
+        && key
+        && language
+    ) {
+        res.status(400).json({
+            "error": "Please, provide text to analyze."
+        });
+    }
 });
 
 export default router;
