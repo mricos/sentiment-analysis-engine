122e159b4c898ce2b4ade0a376c02da65ecd6685
Zach Overhulser <zoverhulser@gmail.com>
2020-09-10 16:32:36 -0500

    Removes comments.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
 import express from "express";
 const router = express.Router();
 
-/* GET home page. */
-//router.get("/", function(req, res, next) {
-//  res.render("index", { title: "Express" });
-//});
-
 router.get("/", function (req, res, next) {
     res.status(200).send("API works");
 });
