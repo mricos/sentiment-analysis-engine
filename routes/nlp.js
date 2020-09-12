49a1207bf8fe5cb310a0b8cf6b34f39db35457be
Zach Overhulser <zoverhulser@gmail.com>
2020-09-11 20:50:27 -0500

    Another try.

--- routes/nlp.js
+++ routes/nlp.js
@@ -6,14 +6,17 @@ const router = express.Router();
 router.post("/analyze/sentiment", function(req, res, next) {
 
     const dataToAnalyze = 
-        typeof(req.body) === "object" 
-	&& req.body.hasOwnProperty("text")
+        typeof(req.body.text) === "string" 
+	|| typeof(req.body.text) === "object" && Array.isArray(req.body.text)
 	    ? req.body.text
 	    : false;
 
+    console.log("Here is the data: ", dataToAnalyze);
+
     // if there's data to analyze
     if (dataToAnalyze) {
-        // if the data comes in the form of an array
+
+	// if the data comes in the form of an array
 	if (Array.isArray(dataToAnalyze)) {
             	
 	    const sentiments = dataToAnalyze.map(createSentiments);
