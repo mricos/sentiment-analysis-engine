ceee6f725176604e5ee0efd181b07febc310c9ef
Zach Overhulser <zoverhulser@gmail.com>
2020-09-13 16:06:03 -0500

    Writes to db when post req data type is string. Gets data from db.

--- routes/nlp.js
+++ routes/nlp.js
@@ -2,7 +2,7 @@ import express from "express";
 import fs from "fs";
 import path from "path";
 const { dirname } = path;
-import createSentiments from "./utils/create-sentiments.js";
+import analyzeSentiments from "./utils/analyze-sentiments.js";
 import { fileURLToPath } from "url";
 const router = express.Router();
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -10,19 +10,29 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const PATHTODB = path.join(__dirname, "../data/sentiments.json");
 
 router.get("/analyze/sentiment/:id", function (req, res, next) {
-    const id = req.params.id 
-    console.log("Here is id: ", id);
-    console.log("Datatype of id: ", typeof(id));
-
-    fs.readFile(
-        PATHTODB,
-	function(err, data) {
-	    if (err) throw err;
-            const db = JSON.parse(data);
-	    console.log("Here is the current db: ", db)
-	}
-    );
     
+    const id = Number(req.params.id) ? req.params.id : false;
+
+    if (id) {
+        fs.readFile(
+            PATHTODB,
+	    function(err, data) {
+	        if (err) throw err;
+                const db = JSON.parse(data);
+	        const sentiments = db[id];
+
+		if (sentiments) {
+		    delete db[id];
+	            res.status(200).json({sentiments});
+		} else {
+		    res.status(400).json({message: `Id ${id} not found.`});
+		}
+	        
+	    }
+        );
+    } else {
+        res.status(400).json({message: "Please, provide id."})
+    }
 });
 
 router.post("/analyze/sentiment", function(req, res, next) {
@@ -41,7 +51,7 @@ router.post("/analyze/sentiment", function(req, res, next) {
 	// if the data comes in the form of an array
 	if (Array.isArray(dataToAnalyze)) {
 
-	    const sentiments = dataToAnalyze.map(createSentiments);
+	    const sentiments = dataToAnalyze.map(analyzeSentiments);
 	    
 	    fs.readFile(
 		PATHTODB,
@@ -69,12 +79,31 @@ router.post("/analyze/sentiment", function(req, res, next) {
 
 	} else if (typeof(dataToAnalyze) === "string") {
 	    
-	    const sentiment = createSentiments(dataToAnalyze);
+	    const sentiments = analyzeSentiments(dataToAnalyze);
 	    
-	    res.status(200).json({
-	        timestamp: new Date(),
-		sentiment
-	    });
+	    fs.readFile(
+		PATHTODB,
+	        function(err, data) {
+	            if (err) throw err;
+		    // Pull object database from file	    
+	            const db = JSON.parse(data);
+	            const id = Date.now();
+		    // Update database
+	            db[id] = {sentiments};
+		    const refreshedDb = JSON.stringify(db);
+	            // Write to database with update
+		    fs.writeFile(
+			PATHTODB,
+			refreshedDb,
+		        function(err) {
+			    if (err) throw err;
+                            res.status(200).json({
+                                id
+                            });
+			}
+		    )
+		}
+	    );
 	
 	} else {
 	    res.status(400).json({
