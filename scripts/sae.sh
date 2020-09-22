449ccfed38eeb56e54a1453fce5df919cec68f29
Zach Overhulser <zoverhulser@gmail.com>
2020-09-21 23:56:26 -0500

    Improves upon functions to test reqs to api.

---
+++ scripts/sae.sh
@@ -0,0 +1,32 @@
+#sae-parse-file() {
+#    jq . < "$1"
+#}
+
+sae-post-data() {
+    local ip="$1";
+    local port="$2";
+    local path="$3"
+    curl -s -X POST \
+    -H "Content-Type: application/json" \
+    -H "Authorization: token" \
+    -d @- \
+    "$ip:$port$path"
+}
+
+sae-get-data-with-id() {
+    local ip="$1";
+    local port="$2";
+    local path="$3";
+    local id="$4";
+    curl -s "$ip:$port$path/$id"
+}
+
+
+sae-parse-file() {
+    jq '.' -s < $1 | jq '.[]."'$2'"' | jq '.['$3':'$4']' -s > data.$$
+    data_for_sae="$(cat ./data.$$)"
+
+    echo '{"text": '"$data_for_sae"'}' > data.sae
+    rm ./data.$$
+    cat ./data.sae
+}
---
+++ scripts/test-api.sh
@@ -0,0 +1,49 @@
+# use: ./test-api.sh > test.results
+
+source ./sae.sh
+
+IP=157.245.233.116;
+PORT=1025;
+DATA=/home/admin/src/sentiment-analysis-engine/data/biden-trump.tweetgen
+
+echo "POST array to API" >> test.results
+sae-parse-file $DATA text | sae-post-data $IP $PORT \
+	/api/nlp/analyze/sentiment > api.results
+array_id="$(jq '.id' < api.results)"
+echo -e "ID received for array data: $array_id\n" >> test.results
+
+echo "GET sentiments for array:$array_id" >> test.results
+sae-get-data-with-id $IP $PORT \
+	/api/nlp/analyze/sentiment "$array_id" > api.results
+array_sentiment="$(jq '.sentiments' < api.results)";
+echo -e "Sentiments for array: $array_sentiment\n" >> test.results
+
+echo "GET original data for array:$array_id" >> test.results
+sae-get-data-with-id $IP $PORT \
+	/api/nlp/data "$array_id" > api.results
+original_array="$(jq '.data' < api.results)" >> test.results
+echo "Original array data for array:$array_id" >> test.results
+echo "$original_array" >> test.results
+
+echo -e "\n\n" >> test.results
+
+echo "POST string to API" >> test.results
+sae-parse-file $DATA text | sae-post-data $IP $PORT \
+	/api/nlp/analyze/sentiment > api.results
+string_id="$(jq '.id' < api.results)"
+echo -e "ID received for string data: $string_id\n" >> test.results
+
+echo "GET sentiment for string:$string_id" >> test.results
+sae-get-data-with-id $IP $PORT \
+	/api/nlp/analyze/sentiment "$string_id" > api.results
+string_sentiment="$(jq '.sentiments' < api.results)";
+echo -e "Sentiment for string: $string_sentiment\n" >> test.results
+
+echo "GET original data for string:$string_id" >> test.results
+sae-get-data-with-id $IP $PORT \
+    /api/nlp/data "$string_id" > api.results
+original_string="$(jq '.data' < api.results)";
+echo "Original string data for string:$string_id" >> test.results
+echo "$original_string" >> test.results
+
+rm api.results
