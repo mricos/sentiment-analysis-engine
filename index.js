082c25379b81deb8209e1e74e7f200dec1cb68ba
Zach Overhulser <zoverhulser@gmail.com>
2020-09-24 18:41:56 -0500

    Removes old file code.

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
     node
 */
 import express from "express";
-// import path from "path";
-// const {dirname} = path;
-// import {fileURLToPath} from "url";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 
@@ -13,18 +10,13 @@ import nlpRouter from "./routes/nlp.js";
 import testRouter from "./routes/tests.js";
 
 const app = express();
-// const __dirname = dirname(fileURLToPath(import.meta.url));
-// console.log("Here is import.meta.url: ", import.meta.url);
-// console.log("Here is dirname: ", __dirname);
 
 app.use(logger("dev"));
 app.use(express.json({limit: "25mb"}));
-app.use(express.urlencoded({limit: "25mb", extended: false}));
+//app.use(express.urlencoded({limit: "25mb", extended: false}));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
 app.use("/api/nlp", nlpRouter);
-app.use("/api/test", testRouter);
+//app.use("/api/test", testRouter);
 
 export default Object.freeze(app);
