85275dbcc3265c56c15462710c3be940d50195fc
Zach Overhulser <zoverhulser@gmail.com>
2020-09-29 16:54:07 -0500

    Fixes baseurl so that data can continue to flow.

--- routes/utils/baseUrl-handler.js
+++ routes/utils/baseUrl-handler.js
@@ -5,6 +5,10 @@ const networkInterfaces = os.networkInterfaces();
 const IP = networkInterfaces["eth0"][0].address
 const { PORT } = process.env
 
-const baseUrlHandler = axios.create({baseURL: `${IP}:${PORT}/api/nlp`});
+const baseUrlHandler = axios.create(
+    {
+	    baseURL: `http://${IP}:${PORT}/api/nlp`
+    }
+);
 
 export default baseUrlHandler; 
