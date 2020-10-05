adf8460ec2df649cbb1e8f93f4b9756ea1357a37
Zach Overhulser <zoverhulser@gmail.com>
2020-10-05 10:20:34 -0500

    Specifies responses.

--- routes/nlp.js
+++ routes/nlp.js
@@ -24,7 +24,17 @@ router.post("/", function(req, res, next) {
 
 // http://IP:PORT/api/nlp/:data
 router.get("/:data", function(req, res, next) {
-    console.log("Here is the url: ", req.url);
+    if (req.url.length > 1) {
+        const data = req.url.replace("-", " ");
+	const sentiment = analyzeSentiments(data);
+	res.status(200).json({
+	    sentiment
+	});
+    } else {
+        res.status(400).json({
+	    message: "Request data must be longer than one character."
+	});
+    }
 });
 
 export default router;
