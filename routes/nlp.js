8d83b858e455d7d764d5f92d85f0369b0c8c51d8
Zach Overhulser <zoverhulser@gmail.com>
2020-10-24 15:58:10 -0500

    Handles array data in POST. Removes GET.

--- routes/nlp.js
+++ routes/nlp.js
@@ -6,34 +6,34 @@ import analyzeSentiments from "./utils/analyze-sentiments.js";
 // http://IP:PORT/api/nlp
 router.post("/", function(req, res, next) {
     // data: String
-    const data = typeof(req.body.data) === "string" 
-	? req.body.data 
-	: false;
-    // if data is String
-    if (data) {
-	const sentiment = analyzeSentiments(data);
-        res.status(200).json({
-	    sentiment
-	});
-    } else {
-        res.status(400).json({
-	    message: "Incorrect request body data type."
-	});
-    }
-});
+    const data = 
+        typeof(req.body.data) === "string" || Array.isArray(req.body.data) 
+	        ? req.body.data 
+	        : false;
 
-// http://IP:PORT/api/nlp/:data
-router.get("/:data", function(req, res, next) {
-    if (req.url.length > 1) {
-        const data = req.url.replace("-", " ");
-	const sentiment = analyzeSentiments(data);
-	res.status(200).json({
-	    sentiment
-	});
+    // if data is String
+    if (typeof(data) === "string") {
+	    
+        const sentiment = analyzeSentiments(data);
+        
+        res.status(200).json({
+	        sentiment
+	    });
+
+    // if data is Array
+    } else if (Array.isArray(data)) {
+
+        const sentiment = data.map(analyzeSentiments);
+
+        res.status(200).json({
+            sentiment
+        });
+
     } else {
+        
         res.status(400).json({
-	    message: "Request data must be longer than one character."
-	});
+	        message: "Incorrect request body data type."
+	    });
     }
 });
 
