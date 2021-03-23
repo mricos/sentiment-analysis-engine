0e805ec91d2c64dde0d14a52c7c8fc9f50706b2c
zoverlvx <zoverhulser@gmail.com>
2021-03-22 19:01:30 -0700

    Adds random words to use. Generates n-amount of random words.

---
+++ scripts/nom-create-random-words.sh
@@ -0,0 +1,15 @@
+nom-create-random-words() {
+  local amount="$1"; # amount of words to create
+  # $2: file from which to find words
+  local words=($(cat "$2"));
+  local len="${#words[@]}"; # generate indices from 0 to len
+  ((len--)) # zero-indexed
+  local random_words=();
+  while [[ "$amount" > 0 ]]; do
+    # generates random index 
+    # uses random index to append word from words array to random_words
+    random_words+=("${words[$(((1 + $RANDOM % "$len")))]}");    
+    ((amount--))
+  done
+  echo "${random_words[*]}"
+}
