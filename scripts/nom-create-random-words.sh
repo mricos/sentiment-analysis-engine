399f3684fe74fb615eb179e418c808e12e1e34a0
zoverlvx <zoverhulser@gmail.com>
2021-03-23 12:07:36 -0700

    Improves readability of the code.

--- scripts/nom-create-random-words.sh
+++ scripts/nom-create-random-words.sh
@@ -1,14 +1,14 @@
 nom-create-random-words() {
   local amount="$1"; # amount of words to create
-  # $2: file from which to find words
-  local words=($(cat "$2"));
+  local words=($(cat "$2")); # $2: file from which to find words
   local len="${#words[@]}"; # generate indices from 0 to len
   ((len--)) # zero-indexed
   local random_words=();
   while [[ "$amount" > 0 ]]; do
     # generates random index 
     # uses random index to append word from words array to random_words
-    random_words+=("${words[$(shuf -i 0-$len -n 1)]}");
+    local i="$(shuf -i 0-$len -n 1)";
+    random_words+=("${words[$i]}");
     ((amount--))
   done
   echo "${random_words[*]}"
