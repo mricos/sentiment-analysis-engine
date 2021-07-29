4e689e9dd0d780813e956d67d8c87e75fa6a2189
zoverlvx <zoverhulser@gmail.com>
2021-07-29 11:43:07 -0700

    Uses jq to get data back.

--- lockdown.sh
+++ lockdown.sh
@@ -1,7 +1,7 @@
 PORT=${PORT:-3000}
 while read line; do
-  curl -X POST\
+  curl -s -X POST\
   -H "Content-Type: application/json"\
   -d '{"data": "'"$line"'"}'\
   'localhost:'"$PORT"'/api/nlp'
-done < lockdown.txt
+done < lockdown.txt | jq '.sentiment'
