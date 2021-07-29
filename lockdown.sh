9592e33480246dec6b8c0570be9cc71377c13d08
zoverlvx <zoverhulser@gmail.com>
2021-07-29 11:35:37 -0700

    Allows for different port number

--- lockdown.sh
+++ lockdown.sh
@@ -1,6 +1,7 @@
+PORT=${PORT:-3000}
 while read line; do
   curl -X POST\
   -H "Content-Type: application/json"\
   -d '{"data": "'"$line"'"}'\
-  'localhost:3000/api/nlp'
+  'localhost:'"$PORT"'/api/nlp'
 done < lockdown.txt
