a8b6afe6f2d4c547166ba414786711bebb2c9ea3
zoverlvx <zoverhulser@gmail.com>
2021-08-10 16:38:20 -0700

    Removes unneeded files

--- lockdown.sh
+++
@@ -1,7 +0,0 @@
-PORT=${PORT:-3000}
-while read line; do
-  curl -s -X POST\
-  -H "Content-Type: application/json"\
-  -d '{"data": "'"$line"'"}'\
-  'localhost:'"$PORT"'/api/nlp'
-done < lockdown.txt | jq '.sentiment'
