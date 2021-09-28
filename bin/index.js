68d7abc6c13a17861e39057642cbd5c7208e4fc4
zoverlvx <zoverhulser@gmail.com>
2021-09-27 20:45:13 -0700

    Allows for CORS

--- bin/index.js
+++ bin/index.js
@@ -5,12 +5,16 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import cors from "cors";
 //import authorizer from "../utils/auth.js";
 
 import nlpRouter from "../routes/nlp.js";
 //import saeRouter from "../routes/sae.js";
 
 const app = express();
+
+// Enable All CORS requests
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json({limit: "25mb"}));
