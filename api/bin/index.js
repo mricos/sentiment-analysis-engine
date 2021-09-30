9f750ea78d91a3382134e303896b494b15383202
zoverlvx <zoverhulser@gmail.com>
2021-09-30 12:57:25 -0700

    Renames files in order to see if it'll work on vercel now.

--- api/bin/index.js
+++ api/bin/index.js
@@ -1,30 +1,107 @@
-/* jslint
-    node
+/* Original example #! as first two characters so the file
+could be run as a full blooded linux command (and not as the
+input to node). But we want to explicitly set runtime, etc,
+and don't want someone to run this file from the command line.
+
+#!/usr/bin/env node
+
 */
 
-import express from "express";
-import cookieParser from "cookie-parser";
-import logger from "morgan";
-import cors from "cors";
-//import authorizer from "../utils/auth.js";
+/* eslint valid-jsdoc: "error" */
 
-import nlpRouter from "../routes/nlp.js";
-//import saeRouter from "../routes/sae.js";
+/**
+ * Module dependencies.
+ */
 
-const app = express();
+import app from "./server.js";
+import debugModule from "debug";
+const debug = debugModule("node-nlp:server");
+import http from "http";
 
-// Enable All CORS requests
-app.use(cors());
+/**
+ * Get port from environment and store in Express.
+ */
 
-app.use(logger("dev"));
-app.use(express.json({limit: "25mb"}));
-//app.use(express.urlencoded({limit: "25mb", extended: false}));
-app.use(cookieParser());
+/**
+ * Normalize a port into a number, string, or false.
+ */
 
-//app.use("/sae", saeRouter);
-app.get("/", function (req, res) {
-    res.json({test: "working"});
-});
-app.use("/api/nlp", nlpRouter);
+function normalizePort(val) {
+    "use strict";
+    const port = parseInt(val, 10);
 
-export default Object.freeze(app);
+    if (Number.isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+const PORT = normalizePort(process.env.PORT || "3000");
+app.set("port", PORT);
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+    "use strict";
+    if (error.syscall !== "listen") {
+        throw error;
+    }
+    const bind = (
+        typeof PORT === "string"
+            ? "Pipe " + PORT
+            : "Port " + PORT
+    );
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+    case "EACCES":
+        console.error(bind + " requires elevated privileges");
+        process.exit(1);
+        break;
+    case "EADDRINUSE":
+        console.error(bind + " is already in use");
+        process.exit(1);
+        break;
+    default:
+        throw error;
+    }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+    "use strict";
+    const addr = server.address();
+    const bind = (
+        typeof addr === "string"
+            ? "pipe " + addr
+            : "port " + addr.port
+    );
+    debug("Listening on " + bind);
+    console.log(`Project SAE running on port: ${PORT}`);
+}
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(PORT);
+server.on("error", onError);
+server.on("listening", onListening);
---
+++ api/bin/server.js
@@ -0,0 +1,30 @@
+/* jslint
+    node
+*/
+
+import express from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import cors from "cors";
+//import authorizer from "../utils/auth.js";
+
+import nlpRouter from "../routes/nlp.js";
+//import saeRouter from "../routes/sae.js";
+
+const app = express();
+
+// Enable All CORS requests
+app.use(cors());
+
+app.use(logger("dev"));
+app.use(express.json({limit: "25mb"}));
+//app.use(express.urlencoded({limit: "25mb", extended: false}));
+app.use(cookieParser());
+
+//app.use("/sae", saeRouter);
+app.get("/", function (req, res) {
+    res.json({test: "working"});
+});
+app.use("/api/nlp", nlpRouter);
+
+export default Object.freeze(app);
