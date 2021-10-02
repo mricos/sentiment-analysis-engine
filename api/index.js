73795cf0f3ad574684024dc7f44822e0ba91c745
zoverlvx <zoverhulser@gmail.com>
2021-10-01 19:24:09 -0700

    Testing in Vercel environment.

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
 module.exports = function (req, res) {
-    res.json({"message": "It's working"})
-};
+    res.status(200).send(
+        "Welcome to SAE!\nPlease, post text to `/api/nlp` for sentiment analysis."
+    );
+}
