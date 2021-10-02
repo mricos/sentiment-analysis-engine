e9158f25c4adacac41a9b42c8eab23f9a20754cb
Zachary Overhulser <zoverhulser@gmail.com>
2021-10-02 22:11:22 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-export default function (req, res) {
+module.exports = function (req, res) {
     res.status(200).send(
         "Welcome to SAE!\nPlease, post text to `/api/nlp` for sentiment analysis."
     );
