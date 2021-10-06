bfc8ecc7d4e3f723ee8a1d75bc0ef4918c23adfc
zoverlvx <zoverhulser@gmail.com>
2021-10-05 20:22:58 -0700

    Fits for Vercel

---
+++ api/nlp.js
@@ -0,0 +1,37 @@
+import analyzeSentiments from "../lib/analyze-sentiments.js";
+
+// http://IP:PORT/api/nlp
+export default function(req, res) {
+    
+    // data: String
+    const data = 
+        typeof(req.body.data) === "string" || Array.isArray(req.body.data) 
+	        ? req.body.data 
+	        : false;
+
+    // if data is String
+    if (typeof(data) === "string") {
+	    
+        const sentiment = analyzeSentiments(data);
+        
+        res.status(200).json({
+	        sentiment
+	    });
+
+    // if data is Array
+    } else if (Array.isArray(data)) {
+
+        const sentiment = data.map(analyzeSentiments);
+
+        res.status(200).json({
+            sentiment
+        });
+
+    } else {
+        
+        res.status(400).json({
+	        message: "Incorrect request body data type.",
+		data: req.body.data
+	    });
+    }
+});
